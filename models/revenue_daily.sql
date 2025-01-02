27326b1e8fa1d377d0c2a4e10f305ac5dbbba82e
vsmoleevskii <vl.smoleevskiy@gmail.com>
2025-01-02 17:54:29 +0400

    Add new model and properties changes

---
+++ models/revenue_daily.sql
@@ -0,0 +1,16 @@
+
+select 
+	sum(price_rub) as revenue_rub,
+	date,
+    now() at time zone 'utc' as updated_at
+from 
+    {{ ref("trips_prep") }} 
+{% if is_incremental() %}
+where
+    date >= (select max(date) - interval '2' day from {{this}})
+{% endif %}
+group by
+    date,
+    updated_at
+order by
+    date
