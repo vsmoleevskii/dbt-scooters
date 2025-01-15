202b4b7b3a0c0b2d0361ac0240205c54a49d8129
vsmoleevskii <vl.smoleevskiy@gmail.com>
2025-01-15 23:20:46 +0400

    Add age group seed and new statistic model by these groups

---
+++ models/trips_age_groups.sql
@@ -0,0 +1,11 @@
+select 
+    ag.group as age_group,
+    count(*) as trips,
+    sum(price_rub) as revenue_rub
+from 
+    {{ref("trips_users")}} as tu
+cross join 
+    {{ref("age_groups")}} as ag
+where 
+    tu.age >=ag.age_start and tu.age<=ag.age_end
+group by age_group
