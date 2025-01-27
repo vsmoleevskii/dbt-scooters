6eadd67c4cab570f03287d1fd7ce27b109c51dee
vsmoleevskii <vl.smoleevskiy@gmail.com>
2025-01-28 00:25:40 +0400

    Clead duplicates in events_clean model after 2023-06-01

--- models/events_clean.sql
+++ models/events_clean.sql
@@ -1,15 +1,20 @@
+{% set date = var("date", none) %}
 select distinct
     user_id,
     timestamp,
     type_id,
-    {{ updated_at() }} 
+    {{ updated_at() }}
 from
-{{ source("scooters_raw", "events") }}
+    {{ source("scooters_raw", "events") }}
 where
 {% if is_incremental() %}
+    {% if date %}
+        date(timestamp) = date '{{ date }}'
+    {% else %}
         timestamp > (select max(timestamp) from {{ this }})
+    {% endif %}
 {% else %}
-        timestamp < timestamp '2023-08-01'
+    timestamp < timestamp '2023-08-01'
 {% endif %}
 
 
